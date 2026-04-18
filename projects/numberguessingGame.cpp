eb3d8497ba638c3ebeb0714b1677d4df66c95734
Nathan Moura <nathanmouramotta@gmail.com>
2026-04-17 23:17:04 -0300

    Added comments to the program

--- projects/numberguessingGame.cpp
+++ projects/numberguessingGame.cpp
@@ -9,22 +9,22 @@ int main(){
 
 srand(time(NULL));
 while(true){
-    std::cout << "Welcome to the number guessing game! Would you like to play? (Y / N)" << std::endl;
+    std::cout << "Welcome to the number guessing game! Would you like to play? (Y / N)" << std::endl;   //Ask the user if he wants to start the program.
     std::cin >> playGame;
     if(playGame == 'Y' || playGame == 'y'){
         std::cout << "Lets get started" << std::endl;
-        break;
+        break; //If the answer is yes (Y) display a message then break the while loop.
     }
         
     else if(playGame == 'N' || playGame =='n'){
         std::cout << "Maybe next time! ";
-        return 0;
+        return 0; //Break the while loop and stop the program.
     }
     else{
         std::cout << "Please put a valid answer (Y or N only)." << std::endl;
     }
 }
- do{
+ do{      //Main game loop
     tries = 0;
     randNum = rand() % 100 + 1;
     do{
@@ -42,10 +42,10 @@ while(true){
 
     }while (guess != randNum);
 
-    std::cout << "Would you like to play again? (Y / N) " << std::endl;
+    std::cout << "Would you like to play again? (Y / N) " << std::endl;  //Ask if the user wants to restart the game
     std::cin >> restartGame;
 
- }while (restartGame == 'Y' || restartGame == 'y');
+ }while (restartGame == 'Y' || restartGame == 'y');  
 
 std::cout << "Thanks for playing! " << std::endl;
 return 0;
